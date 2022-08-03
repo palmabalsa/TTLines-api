ff8a603ad05ebaf43eaffe9f14a686a6966f01de
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-08-03 12:36:59 +1200

    update-trial-endpoint

--- troutApi/urls.py
+++ troutApi/urls.py
@@ -4,7 +4,7 @@ from .views import EditOrDeleteLogEntry, FishList, CreateLogEntry, TrialEndPoint
 app_name = 'troutApi'
 
 urlpatterns = [
-    path('log/all/', TrialEndPointFishList.as_view(), name = 'trial'),
+    path('all/', TrialEndPointFishList.as_view(), name = 'trial'),
     path('log/', FishList.as_view(), name = 'fishingloglist'),
     path('create/', CreateLogEntry.as_view(), name = 'create'),
     path('log/<int:id>', EditOrDeleteLogEntry.as_view(), name= 'updatedelete'),
