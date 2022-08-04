1e3c096f56f12dd14d382b1a381a82f8e9e5b997
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-08-05 11:30:51 +1200

    update cors allowed origins

--- tight_lines/settings.py
+++ tight_lines/settings.py
@@ -191,8 +191,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost",
     "http://localhost:55555",
 ]
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = list(default_methods)
 CORS_ALLOW_HEADERS = list(default_headers)
 
