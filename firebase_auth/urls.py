98b66be2f8eb750ef1df58c94e8b336589e49540
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-08-05 14:02:21 +1200

    update firebase auth setting

--- firebase_auth/urls.py
+++ firebase_auth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from firebase_auth.views import FireBaseAuthRegister
+from firebase_auth.views import FireBaseAuthRegister, FireBaseAuthAPI, protected, public
 
 app_name = 'firebase_auth' 
 
@@ -7,8 +7,9 @@ app_name = 'firebase_auth'
    
    
 urlpatterns = [
-    path('auth/', FireBaseAuthRegister.as_view(), name='firebase/auth')
-    # path('protected/', protected, name='protected'),
-    # path('public/', public, name='public'),
+    path('auth/', FireBaseAuthRegister.as_view(), name='firebase/auth'),
+    path('signup/', FireBaseAuthAPI.as_view(), name='firebase/auth'),
+    path('protected/', protected, name='protected'),
+    path('public/', public, name='public'),
 ]
 
--- firebase_auth/views.py
+++ firebase_auth/views.py
@@ -51,70 +51,70 @@ class FireBaseAuthRegister(GenericAPIView):
     
 
 
-# class FireBaseAuthAPI(GenericAPIView):
-#     serializer_class = FireBaseAuthSerializer
+class FireBaseAuthAPI(GenericAPIView):
+    serializer_class = FireBaseAuthSerializer
     
-#     def post(
-#         self: "FireBaseAuthAPI",
-#         request: Request,
-#         *args: Tuple,
-#         **kwargs: Dict
-#     ) -> Response:
-#         serializer = self.get_serializer(data=request.data)
+    def post(
+        self: "FireBaseAuthAPI",
+        request: Request,
+        *args: Tuple,
+        **kwargs: Dict
+    ) -> Response:
+        serializer = self.get_serializer(data=request.data)
         
-#         if serializer.is_valid():
-#             id_token = serializer.data.get('token')
+        if serializer.is_valid():
+            id_token = serializer.data.get('token')
             
-#             try: 
-#                 decoded_token = auth.verify_id_token(id_token)
+            try: 
+                decoded_token = auth.verify_id_token(id_token)
                 
-#             except Exception:
-#                 raise APIException(
-#                     detail= 'Invalid token',
-#                     code=status.HTTP_400_BAD_REQUEST
-#                 )
-#             try:
-#                 firebase_user_id = decoded_token['uid']
-#             except KeyError:
-#                 raise APIException(
-#                     detail='No such User',
-#                     code=status.HTTP_400_BAD_REQUEST
-#                 )
-#             try:
-#                 user = User.objects.get(firebase_user_id=firebase_user_id)
-#                 content = {
-#                     "username" :user.email,
-#                     "email" : user.email,
-#                     "firebase_user_id" : user.firebase_user_id
-#                 }
-#                 return Response(content, status=status.HTTP_200_OK)
-#             except User.DoesNotExist:
-#                 user = auth.get_user(firebase_user_id)
+            except Exception:
+                raise APIException(
+                    detail= 'Invalid token',
+                    code=status.HTTP_400_BAD_REQUEST
+                )
+            try:
+                firebase_user_id = decoded_token['uid']
+            except KeyError:
+                raise APIException(
+                    detail='No such User',
+                    code=status.HTTP_400_BAD_REQUEST
+                )
+            try:
+                user = User.objects.get(firebase_user_id=firebase_user_id)
+                content = {
+                    "username" :user.email,
+                    "email" : user.email,
+                    "firebase_user_id" : user.firebase_user_id
+                }
+                return Response(content, status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+                user = auth.get_user(firebase_user_id)
                 
-#                 try:
-#                     new_user = User.objects.create(
-#                         email= user.email,
-#                         firebase_user_id= firebase_user_id,
-#                     )
-#                     content = {
-#                         "email":new_user.email,
-#                         "firebase_user_id": new_user.firebase_user_id
-#                     }
-#                     return Response(content, status=status.HTTP_201_CREATED)
-#                 except IntegrityError:
-#                     raise APIException(
-#                         detail='User already exists',
-#                         code=status.HTTP_400_BAD_REQUEST
-#                     )
+                try:
+                    new_user = User.objects.create(
+                        email= user.email,
+                        firebase_user_id= firebase_user_id,
+                    )
+                    content = {
+                        "email":new_user.email,
+                        "firebase_user_id": new_user.firebase_user_id
+                    }
+                    return Response(content, status=status.HTTP_201_CREATED)
+                except IntegrityError:
+                    raise APIException(
+                        detail='User already exists',
+                        code=status.HTTP_400_BAD_REQUEST
+                    )
                     
 
 
-# @api_view()
-# def public(request: Request) -> Response:
-#     return Response({"message": "Hello, User X"})
+@api_view()
+def public(request: Request) -> Response:
+    return Response({"message": "Hello, User X"})
     
-# @api_view()
-# @permission_classes([permissions.IsAuthenticated])
-# def protected(request: Request) -> Response:
-#     return Response({"message": f"Hello, {request.user}"})     
+@api_view()
+@permission_classes([permissions.IsAuthenticated])
+def protected(request: Request) -> Response:
+    return Response({"message": f"Hello, {request.user}"})     
 
