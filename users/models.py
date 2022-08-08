27d556734f341c088ff80e24c6e6e2c06592fc01
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-08-08 15:17:43 +1200

    user model manager

--- users/models.py
+++ users/models.py
@@ -7,37 +7,37 @@ from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManag
 import jwt
 from django.conf import settings
 # Create your models here.
-# class CustomUserManager(UserManager):
+class CustomUserManager(UserManager):
     
-#     use_in_migrations: True
+    use_in_migrations: True
     
-#     def _create_user(self, username, email, password, **extra_fields): 
-#         if not username: 
-#             raise ValueError(_('Username required'))   
-#         if not email: 
-#             raise ValueError(_('Email required'))
-#         email = self.normalize_email(email)
-#         username = self.model.normalize_username(username)
-#         user = self.model(email=email, username=username,**extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
+    def _create_user(self, username, email, password, **extra_fields): 
+        if not username: 
+            raise ValueError(_('Username required'))   
+        if not email: 
+            raise ValueError(_('Email required'))
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(email=email, username=username,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
     
-#     def create_user(self, username, email, password, **extra_fields):
-#         extra_fields.setdefault("is_staff", False)
-#         extra_fields.setdefault("is_superuser", False)
-#         return self._create_user(username, email, password, **extra_fields)
+    def create_user(self, username, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
         
-#     def create_superuser(self, username, email, password, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         extra_fields.setdefault('is_active', True)
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
         
-#         if extra_fields.get('is_staff') is not True:
-#             raise ValueError(_('Superuser must have is_staff=True'))
-#         if extra_fields.get('is_superuser') is not True:
-#             raise ValueError(_('Superuser must have is_superuser=True'))
-#         return self.create_user(username, email, password, **extra_fields)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True'))
+        return self.create_user(username, email, password, **extra_fields)
     
     
     
