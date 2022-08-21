553ff22f68214742202e244832c68a84b092497b
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-08-22 11:21:22 +1200

    edit fish_condition in models

--- trout/models.py
+++ trout/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
-# create a model manager????
-
 class FishingLogEntry (models.Model):
 
     fishSpecies = (
@@ -11,7 +9,7 @@ class FishingLogEntry (models.Model):
         ('Brown', 'Brown'),
     )
     fishCondition = (
-        ('Sick/Spawning', 'Sick or spawning'),
+        ('Sick/Spawning', 'Sick/Spawning'),
         ('Average', 'Average'),
         ('Healthy', 'Healthy'),
         ('Sashimi', 'Sashimi')
@@ -21,30 +19,26 @@ class FishingLogEntry (models.Model):
         ('Tauranga Taupo', 'Tauranga Taupo'),
         ('Lake O', 'Lake O'),
     )
-        #  to_field='firebase_user_id'
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, to_field='firebase_user_id', default='noFirebaseUserID', on_delete=models.CASCADE)
     date = models.DateTimeField(default=timezone.now)
     river = models.CharField(choices = riverName, max_length=14, default=None, blank=True, null=True,)
     river_pool = models.CharField(verbose_name= 'what pool?', max_length=20)
     river_geotag = models.CharField(verbose_name= 'geotag coordinates option here', max_length=20, default=None, blank=True, null=True)
-    river_level = models.DecimalField(decimal_places=2,max_digits=5, default=None, blank=True, null=True) #these 2 want to be populated w my weather data
-    air_pressure = models.DecimalField(decimal_places=2,max_digits=5, default=None, blank=True, null=True) #these 2 want to be populated w my weather data
+    river_level = models.DecimalField(decimal_places=2,max_digits=5, default=None, blank=True, null=True)
+    air_pressure = models.DecimalField(decimal_places=2,max_digits=5, default=None, blank=True, null=True)
     fish_species = models.CharField(choices=fishSpecies, max_length=7, default=None, blank=True, null=True,)
     fish_weight = models.DecimalField(decimal_places=2,max_digits=5, default=None, blank=True, null=True)
     fish_length = models.DecimalField(decimal_places=2,max_digits=5, default=None, blank=True, null=True)
     fish_condition = models.CharField(choices=fishCondition, max_length=13, default=None, blank=True, null=True, )
-    kept_or_released = models.BooleanField(verbose_name='Kept for dinner?', default=None, blank=True, null=True,) #how to implemet this?
+    kept_or_released = models.BooleanField(verbose_name='Kept for dinner?', default=None, blank=True, null=True,)
     fly_used = models.CharField(max_length=25)
     any_notes = models.CharField(max_length=100)
-    # whats this ? is it necessary?
-    objects = models.Manager()#default manager
-    
-    
+
+    objects = models.Manager()
     
     class Meta:
           ordering = ('date',)
     
     def __str__(self):
         return self.fish_species
-    # ^this means youll be able to see this is the admin dashboard
-    # what to identify your database via - good question, what would mine be? date?
