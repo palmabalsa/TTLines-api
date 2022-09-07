476349f203be36ad2cd655e83fa43e0e9cf469a8
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-09-08 08:46:29 +1200

    fish_condition field-change options > spent/excellent

--- trout/models.py
+++ trout/models.py
@@ -9,10 +9,11 @@ class FishingLogEntry (models.Model):
         ('Brown', 'Brown'),
     )
     fishCondition = (
-        ('Sick/Spawning', 'Sick/Spawning'),
+        # ('Sick/Spawning', 'Sick/Spawning'),
+        ('Spent', 'Spent'),
         ('Average', 'Average'),
-        ('Healthy', 'Healthy'),
-        ('Sashimi', 'Sashimi')
+        ('Good', 'Good'),
+        ('Sashimi(Excellent)', 'Sashimi(Excellent)')
     )
     riverName = (
         ('Tongariro', 'Tongariro'),
@@ -31,7 +32,7 @@ class FishingLogEntry (models.Model):
     fish_species = models.CharField(choices=fishSpecies, max_length=7, default=None, blank=True, null=True,)
     fish_weight = models.DecimalField(decimal_places=2,max_digits=5, default=None, blank=True, null=True)
     fish_length = models.DecimalField(decimal_places=2,max_digits=5, default=None, blank=True, null=True)
-    fish_condition = models.CharField(choices=fishCondition, max_length=13, default=None, blank=True, null=True, )
+    fish_condition = models.CharField(choices=fishCondition, max_length=18, default=None, blank=True, null=True, )
     kept_or_released = models.BooleanField(verbose_name='Kept for dinner?', default=None, blank=True, null=True,)
     fly_used = models.CharField(max_length=25,default=None, blank=True, null=True)
     any_notes = models.CharField(max_length=100, default=None, blank=True, null=True)
